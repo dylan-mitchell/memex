40cb99012bc6a6d0d6f7f6c8576ad7a66b1ea62d
dylan-mitchell <djmitc04@louisville.edu>
2020-02-18 15:28:39 -0500

    Removed menu and scrollbar

--- main.js
+++ main.js
@@ -161,6 +161,8 @@ function createWindow() {
     }
   });
 
+	mainWindow.setMenu(null);
+
   if (fs.existsSync(path.join(__dirname, "dataserver", "takeout.db"))) {
     mainWindow.loadFile(path.join(__dirname, "src", "home.html"));
   } else {
@@ -175,16 +177,10 @@ app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function() {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") app.quit();
+	server.kill(0)
 });
 
 app.on("activate", function() {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
